e58566625f95240c8fc26ee8e4adb7af9d68f282
WEI-TSUNG CHENG <forwindsun1359@gmail.com>
2026-02-08 19:18:40 +0000

    chapter6

--- rango/admin.py
+++ rango/admin.py
@@ -2,8 +2,12 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
+class CategoryAdmin(admin.ModelAdmin):
+    populated_fields = {'slug': ('name',)}
+
 admin.site.register(Page, PageAdmin)
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
