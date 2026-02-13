c728a507f1e414818eedbe992c7a0a40c0887131
WEI-TSUNG CHENG <forwindsun1359@gmail.com>
2026-02-13 13:57:19 +0000

    chapter7 exercise

--- rango/views.py
+++ rango/views.py
@@ -3,10 +3,13 @@ from unicodedata import category
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-from rango.forms import CategoryForm
+
 from rango.models import Category
+from rango.forms import CategoryForm
 
 from rango.models import Page
+from rango.forms import PageForm
+from django.urls import reverse
 
 def index(request):
     # context_dict = {
@@ -58,3 +61,34 @@ def add_category(request):
             print(form.errors)
 
     return render(request, 'rango/add_category.html', {'form' : form })
+
+def add_page(request, category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category = None
+
+    if category is None:
+        return redirect('/rango/')
+
+    form = PageForm()
+
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+
+        if form.is_valid():
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+                # 如果 Page 添加成功 => 回到上ㄧ層的 Category 頁
+                # 透過 rango:show_category 名稱在 urls.py 找到對應路徑
+                # 因為 找到的路經需要執行的方法是 show_category() 所以需要提供必要參數放在 kwargs
+                return redirect(reverse('rango:show_category', kwargs={'category_name_slug': category_name_slug}))
+        else:
+            print(form.errors)
+
+    context_dict = {'form': form, 'category': category}
+    # 其他情形都回到 添加 Page 頁
+    return render(request, 'rango/add_page.html', context=context_dict)
