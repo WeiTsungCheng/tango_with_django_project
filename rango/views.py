c7d2f1f1dfb467990fed62186e260ae05a347b77
WEI-TSUNG CHENG <forwindsun1359@gmail.com>
2026-02-13 01:57:57 +0000

    chapter7

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,9 @@
 from unicodedata import category
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+from rango.forms import CategoryForm
 from rango.models import Category
 
 from rango.models import Page
@@ -43,3 +45,16 @@ def show_category(request, category_name_slug):
 
     return render(request, 'rango/category.html', context_dict)
 
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('/rango/')
+        else:
+            print(form.errors)
+
+    return render(request, 'rango/add_category.html', {'form' : form })
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -21,9 +21,11 @@ from rango import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rango.views import add_category
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
-    path('rango/', include('rango.urls')),
+    path('rango/', include('rango.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
