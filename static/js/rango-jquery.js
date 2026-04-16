c1e451d45f49c24d0de17f45cfff8f37de73f3e4
WEI-TSUNG CHENG <3118197C@student.gla.ac.uk>
2026-04-16 02:01:33 +0100

    chapter 16 done

---
+++ static/js/rango-jquery.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    alert('Hello, world!');
+});
+
+$(document).ready(function() {
+
+    $('#about-btn').click(function() {
+        alert('Your clicked the button using JQuery !!!!');
+        msgStr = $('#msg').html();
+        msgStr = msgStr + ' ooo, fancy!';
+        $('#msg').html(msgStr);
+    })
+
+    $('p').hover(
+        function() {
+            $(this).css('color', 'red');
+        },
+        function() {
+            $(this).css('color', 'black');
+        }
+    );
+
+
+})
+
+
